4b50e02b5bf630970870836bd6c931a08d4ba462
Git_KPLN <bim@kpln.ru>
2021-10-08 18:37:45 +0300

    Добавлен KPLN_Tools

---
+++ KPLN_Tools/KPLN_Tools/ModuleData.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace KPLN_Tools
+{
+    public static class ModuleData
+    {
+#if Revit2020
+        public static string RevitVersion = "2020";
+        public static Window RevitWindow { get; set; }
+#endif
+#if Revit2018
+        public static string RevitVersion = "2018";
+#endif
+        public static System.IntPtr MainWindowHandle { get; set; }//Главное окно Revit (WPF: Для определения свойства .Owner)
+        public static string Build = string.Format("Revit {0}", RevitVersion);
+        public static string Version = "1.0.0.1";//Отображаемая версия модуля в Revit
+        public static string Date = "2021/04/16";//Дата последнего изменения
+        public static string ManualPage = "https://kpln.kdb24.ru/article/60264/";//Ссылка на web-страницу по клавише F1
+        public static string ModuleName = "Tools";//Имя модуля
+
+    }
+}
---
+++ KPLN_Tools/KPLN_Tools/Source/Source.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static KPLN_Tools.Common.Collections;
+
+namespace KPLN_Tools.Source
+{
+    public class Source
+    {
+        public string Value { get; }
+        private static string AssemblyPath = new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName;
+        public Source(Icon icon)
+        {
+            switch (icon)
+            {
+                case Icon.toolBox:
+                    Value = Path.Combine(AssemblyPath, @"Source\ImageData\toolBox.png");
+                    break;
+                case Icon.pushPin:
+                    Value = Path.Combine(AssemblyPath, @"Source\ImageData\pushPin.png");
+                    break;
+                case Icon.renamerFunc:
+                    Value = Path.Combine(AssemblyPath, @"Source\ImageData\renamerFunc.png");
+                    break;
+                default:
+                    throw new Exception("Undefined icon!");
+            }
+        }
+    }
+
+}
