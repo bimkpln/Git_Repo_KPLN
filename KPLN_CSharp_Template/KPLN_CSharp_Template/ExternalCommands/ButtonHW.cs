50aec866f3863ce7cc793968b65f5b3c35b9895d
bimManager <bim.kpln@gmail.com>
2022-06-03 18:48:01 +0300

    KPLN_ModelChecker_User
    Дебаг, и добавил инструкцию по созданию и локальному копированию шаблонов

---
+++ KPLN_CSharp_Template/KPLN_CSharp_Template/ExternalCommands/ButtonHW.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPLN_CSharp_Template.ExternalCommands
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class ButtonHW : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Получение объектов приложения и документа
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            //Тест
+            TaskDialog.Show("wOw", "whazuuup from button", TaskDialogCommonButtons.Ok);
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ KPLN_CSharp_Template/KPLN_CSharp_Template/ExternalCommands/PullDownHW.cs
@@ -0,0 +1,28 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPLN_CSharp_Template.ExternalCommands
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class PullDownHW : IExternalCommand
+    {
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Получение объектов приложения и документа
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            //Тест
+            TaskDialog.Show("wOw", "whazuuup from pulldown", TaskDialogCommonButtons.Ok);
+            return Result.Succeeded;
+        }
+    }
+}
