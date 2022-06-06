9cc0dbf28010e4dee2c4f708721259cb4ddb2089
bimManager <bim.kpln@gmail.com>
2022-06-06 13:02:37 +0300

    KPLN_ModelChecker_User, KPLN_ModelChecker_Debugger
    KPLN_ModelChecker_User - дебаг
    KPLN_ModelChecker_Debugger - первая выгрузка

---
+++ KPLN_ModelChecker_Debugger/KPLN_ModelChecker_Debugger/ExternalCommands/Picker.cs
@@ -0,0 +1,73 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPLN_ModelChecker_Debugger.ExternalCommands
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Picker : IExternalCommand
+    {
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Получение объектов приложения и документа
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            //Список используемых категорий
+            List<BuiltInCategory> builtInCatList = new List<BuiltInCategory>() { 
+                BuiltInCategory.OST_Grids,
+                BuiltInCategory.OST_Levels,
+                BuiltInCategory.OST_ProjectBasePoint,
+                BuiltInCategory.OST_RvtLinks
+            };
+
+            //Элементы из списка категорий
+            int cnt = 0;
+            Transaction trans = new Transaction(doc);
+            trans.Start("KPLN: Прикрепить элементы");
+            foreach (BuiltInCategory curBuiltIn in builtInCatList)
+            {
+                IList<Element> elemColl = new FilteredElementCollector(doc)
+                    .OfCategory(curBuiltIn)
+                    .WhereElementIsNotElementType()
+                    .ToElements();
+
+                foreach (Element curElem in elemColl)
+                {
+                    if (!curElem.Pinned)
+                    {
+                        try
+                        {
+                            curElem.get_Parameter(BuiltInParameter.ELEMENT_LOCKED_PARAM).Set(1);
+                            cnt++;
+                        }
+                        catch (Exception exc)
+                        {
+                            TaskDialog.Show("Ошибка: ", exc.ToString(), TaskDialogCommonButtons.Close);
+                        }
+
+                    }
+
+                }
+            }
+            trans.Commit();
+            if (cnt == 0)
+            {
+                TaskDialog.Show("Итог", "Все было прикреплено до запуска", TaskDialogCommonButtons.Ok);
+            }
+            else
+            {
+                string msg = "Прикреплено " + cnt + " элементов!";
+                TaskDialog.Show("Итог", msg, TaskDialogCommonButtons.Ok);
+            }
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ KPLN_ModelChecker_Debugger/KPLN_ModelChecker_Debugger/Module.cs
@@ -0,0 +1,109 @@
+﻿using Autodesk.Revit.UI;
+using KPLN_Loader.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using static KPLN_Loader.Output.Output;
+
+namespace KPLN_ModelChecker_Debugger
+{
+    public class Module : IExternalModule
+    {
+        private readonly string _AssemblyPath = Assembly.GetExecutingAssembly().Location;
+
+        public Result Close()
+        {
+            return Result.Succeeded;
+        }
+
+        public Result Execute(UIControlledApplication application, string tabName)
+        {
+            //Добавляю кнопку в панель
+            string currentPanelName = "Проверка";
+            RibbonPanel currentPanel = application.GetRibbonPanels(tabName).Where(i => i.Name == currentPanelName).ToList().First();
+
+            //Добавляю выпадающий список pullDown
+            PulldownButtonData pullDownData = new PulldownButtonData("Исправление", "Исправление");
+            PulldownButton pullDown = currentPanel.AddItem(pullDownData) as PulldownButton;
+            BtnImagine(pullDown, "mainIcon.png");
+
+            //Добавляю кнопку в выпадающий список pullDown
+            AddPushButtonDataInPullDown(
+                "Прикрепить",
+                "Прикрепить элементы модели",
+                "Прикрепляет (pin) следующие элементы: связи, оси, уровни, базовую точку проекта",
+                string.Format(
+                    "Прикрпление необходимо для избежания случайного перемещения объектов, что может привести к проектным ошибкам.\nДата сборки: {0}\nНомер сборки: {1}\nИмя модуля: {2}",
+                    ModuleData.Date,
+                    ModuleData.Version,
+                    ModuleData.ModuleName
+                ),
+                typeof(ExternalCommands.Picker).FullName,
+                pullDown,
+                "pushPin.png",
+                "http://moodle.stinproject.local/mod/page/view.php?id=189"
+            );
+
+            return Result.Succeeded;
+        }
+
+        /// <summary>
+        /// Метод для добавления кнопки в выпадающий список
+        /// </summary>
+        /// <param name="name">Внутреннее имя кнопки</param>
+        /// <param name="text">Имя, видимое пользователю</param>
+        /// <param name="shortDescription">Краткое описание, видимое пользователю</param>
+        /// <param name="longDescription">Полное описание, видимое пользователю при залержке курсора</param>
+        /// <param name="className">Имя класса, содержащего реализацию команды</param>
+        /// <param name="pullDownButton">Выпадающий список, в который добавляем кнопку</param>
+        /// <param name="imageName">Имя иконки</param>
+        /// <param name="contextualHelp">Ссылка на web-страницу по клавише F1</param>
+        private void AddPushButtonDataInPullDown(string name, string text, string shortDescription, string longDescription, string className, PulldownButton pullDownButton, string imageName, string contextualHelp)
+        {
+            PushButtonData data = new PushButtonData(name, text, _AssemblyPath, className);
+            PushButton button = pullDownButton.AddPushButton(data) as PushButton;
+            button.ToolTip = shortDescription;
+            button.LongDescription = longDescription;
+            button.ItemText = text;
+            button.SetContextualHelp(new ContextualHelp(ContextualHelpType.Url, contextualHelp));
+            BtnImagine(button, imageName);
+        }
+
+        /// <summary>
+        /// Метод для добавления отдельной в панель
+        /// </summary>
+        /// <param name="name">Внутреннее имя кнопки</param>
+        /// <param name="text">Имя, видимое пользователю</param>
+        /// <param name="shortDescription">Краткое описание, видимое пользователю</param>
+        /// <param name="longDescription">Полное описание, видимое пользователю при залержке курсора</param>
+        /// <param name="className">Имя класса, содержащего реализацию команды</param>
+        /// <param name="panel">Панель, в которую добавляем кнопку</param>
+        /// <param name="imageName">Имя иконки</param>
+        /// <param name="contextualHelp">Ссылка на web-страницу по клавише F1</param>
+        private void AddPushButtonDataInPanel(string name, string text, string shortDescription, string longDescription, string className, RibbonPanel panel, string imageName, string contextualHelp)
+        {
+            PushButtonData data = new PushButtonData(name, text, _AssemblyPath, className);
+            PushButton button = panel.AddItem(data) as PushButton;
+            button.ToolTip = shortDescription;
+            button.LongDescription = longDescription;
+            button.ItemText = text;
+            BtnImagine(button, imageName);
+            button.SetContextualHelp(new ContextualHelp(ContextualHelpType.Url, contextualHelp));
+        }
+
+        /// <summary>
+        /// Метод для добавления иконки для кнопки
+        /// </summary>
+        /// <param name="button">Кнопка, куда нужно добавить иконку</param>
+        /// <param name="imageName">Имя иконки с раширением</param>
+        private void BtnImagine(RibbonButton button, string imageName)
+        {
+            string imageFullPath = Path.Combine(new FileInfo(_AssemblyPath).DirectoryName, @"Imagens\", imageName);
+            button.LargeImage = new BitmapImage(new Uri(imageFullPath));
+
+        }
+    }
+}
