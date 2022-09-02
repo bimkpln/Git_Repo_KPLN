a8b0e00f2a88f4d9686dcf6b3bf274f4ffd522bd
bimManager <bim.kpln@gmail.com>
2022-09-02 17:40:42 +0300

    KPLN_Library_SelectItem. Первая выгрузка

---
+++ 01_KPLN_Library/KPLN_Library_SelectItem/KPLN_Library_SelectItem/Forms/FormSinglePick.xaml.cs
@@ -0,0 +1,75 @@
+﻿using KPLN_Library_DataBase.Collections;
+using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Linq;
+using static KPLN_Loader.Output.Output;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace KPLN_Library_SelectItem.Forms
+{
+    public partial class FormSinglePick : Window
+    {
+        private IEnumerable _collection;
+
+        public DbProject SelectedDbProject;
+
+        public FormSinglePick(IEnumerable collection)
+        {
+            _collection = collection;
+            InitializeComponent();
+
+            Projects.ItemsSource = _collection;
+            PreviewKeyDown += new KeyEventHandler(HandleEsc);
+        }
+
+        private void HandleEsc(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                this.DialogResult = false;
+                Close();
+            }
+        }
+
+        private void OnElementClick(object sender, RoutedEventArgs e)
+        {
+            SelectedDbProject = (sender as Button).DataContext as DbProject;
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        /// <summary>
+        /// Фильтрация по имени
+        /// </summary>
+        private void SearchText_Changed(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string _searchName = textBox.Text.ToLower();
+
+            ObservableCollection<DbProject> filteredProjects = new ObservableCollection<DbProject>();
+
+            foreach (DbProject project in _collection)
+            {
+                if (project.Name.ToLower().StartsWith(_searchName))
+                {
+                    filteredProjects.Add(project);
+                }
+            }
+            
+            Projects.ItemsSource = filteredProjects;
+        }
+
+        /// <summary>
+        /// Запуск без фильтрации
+        /// </summary>
+        private void OnRunWithoutFilterClick(object sender, RoutedEventArgs e)
+        {
+            SelectedDbProject = null;
+            this.DialogResult = true;
+            this.Close();
+        }
+    }
+}
---
+++ 01_KPLN_Library/KPLN_Library_SelectItem/KPLN_Library_SelectItem/SelectProject.cs
@@ -0,0 +1,49 @@
+﻿using KPLN_Library_DataBase;
+using KPLN_Library_DataBase.Collections;
+using KPLN_Library_DataBase.Controll;
+using KPLN_Library_SelectItem.Forms;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+
+namespace KPLN_Library_SelectItem
+{
+    /// <summary>
+    /// Выбор проекта из базы данных
+    /// </summary>
+    public static class SelectProject
+    {
+
+        /// <summary>
+        /// Запуск окна выбора проекта
+        /// </summary>
+        /// <returns>Возвращает выбранный проект, или null, если нужно выбрать всё</returns>
+        /// <exception cref="Exception"></exception>
+        public static FormSinglePick CreateForm()
+        {
+            DbControll.Update();
+            
+            ObservableCollection<DbProject> projects = new ObservableCollection<DbProject>();
+            foreach (DbProject prj in DbControll.Projects)
+            {
+                if (prj.Name.Equals(null))
+                {
+                    throw new Exception($"KPLN_Exception: Ошибка в заполнении БД - у элемента проекта нет имена");
+                }
+                else if (prj.Code.Equals(null))
+                {
+                    throw new Exception($"KPLN_Exception: Ошибка в заполнении БД - у элемента проекта нет имена");
+                }
+                else if (prj.Code != "BIM")
+                {
+                    projects.Add(prj);
+                }
+            }
+
+            FormSinglePick _pickForm = new FormSinglePick(projects.OrderBy(p => p.Name));
+
+            return _pickForm;
+        }
+    }
+}
