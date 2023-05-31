238a21fcf3f3ba7986cf83f6aed51d9bc1af1217
bimManager <bim.kpln@gmail.com>
2023-05-31 13:25:33 +0300

    Library 1.1.2. ExtensibleStorageBuilder - уточнения
     Добавлены методы и документация

--- 01_KPLN_Library/KPLN_Library_ExtensibleStorage/KPLN_Library_ExtensibleStorage/ExtensibleStorageBuilder.cs
+++ 01_KPLN_Library/KPLN_Library_ExtensibleStorage/KPLN_Library_ExtensibleStorage/ExtensibleStorageBuilder.cs
@@ -9,6 +9,14 @@ namespace KPLN_Library_ExtensibleStorage
     /// </summary>
     public class ExtensibleStorageBuilder
     {
+        /// <summary>
+        /// Основной разделитель между данными, которые пишуться в ExtensibleStorage
+        /// </summary>
+        private string _mainDataSeparator = "~ZhvBlr~";
+
+        /// <summary>
+        /// Конструктор для плагинов на python (там своя библиотека, и имя там дефолтное)
+        /// </summary>
         public ExtensibleStorageBuilder(Guid guid, string name)
         {
             Guid = guid;
@@ -21,64 +29,25 @@ namespace KPLN_Library_ExtensibleStorage
             StorageName = storageName;
         }
 
+        /// <summary>
+        /// Guid ExtensibleStorage
+        /// </summary>
         public Guid Guid { get; private set; }
 
+        /// <summary>
+        /// Имя поля в ExtensibleStorage
+        /// </summary>
         public string FieldName { get; private set; }
 
+        /// <summary>
+        /// Имя ExtensibleStorage
+        /// </summary>
         public string StorageName { get; private set; }
 
-        public void SetStorageData_TimeRunLog(Element elem, string userName, DateTime dateTime)
-        {
-            Schema sch;
-            Entity entity = CheckStorageExists(elem);
-            if (entity == null)
-            {
-                sch = CreateSchema();
-                entity = new Entity(sch);
-            }
-
-            entity.Set<string>(FieldName, $"{userName}: {dateTime}");
-            elem.SetEntity(entity);
-        }
-
-        public void SetStorageData_TextLog(Element elem, string userName, string descr)
-        {
-            Schema sch;
-            Entity entity = CheckStorageExists(elem);
-            if (entity == null)
-            {
-                sch = CreateSchema();
-                entity = new Entity(sch);
-            }
-
-            entity.Set<string>(FieldName, $"{userName}: {descr}");
-            elem.SetEntity(entity);
-        }
-
-        public ResultMessage GetResMessage_ProjectInfo(Document doc)
-        {
-            ProjectInfo pi = doc.ProjectInformation;
-            Entity entity = CheckStorageExists((Element)pi);
-            if (entity != null)
-            {
-                ResultMessage message = new ResultMessage()
-                {
-                    CurrentStatus = MessageStatus.Ok,
-                    Description = $"{entity.Get<string>(FieldName)}",
-                };
-                return message;
-            }
-            else
-            {
-                ResultMessage message = new ResultMessage()
-                {
-                    CurrentStatus = MessageStatus.Error,
-                    Description = $"Данные отсутсвуют (не запускался)",
-                };
-                return message;
-            }
-        }
-
+        /// <summary>
+        /// Возвращает спец. класс ResultMessage из ExtensibleStorage для элемента
+        /// </summary>
+        /// <param name="elem">Элемент, из которого получаем ResultMessage</param>
         public ResultMessage GetResMessage_Element(Element elem)
         {
             Schema sch = Schema.Lookup(Guid);
@@ -95,7 +64,7 @@ namespace KPLN_Library_ExtensibleStorage
                             return new ResultMessage()
                             {
                                 CurrentStatus = MessageStatus.Ok,
-                                Description = $"{entity.Get<string>(FieldName)}",
+                                Description = CleanDataByMainSeparator(entity.Get<string>(FieldName)),
                             };
                     }
                 }
@@ -109,8 +78,84 @@ namespace KPLN_Library_ExtensibleStorage
         }
 
         /// <summary>
-        /// Проверяет, есть ли хоть какие-то данные в ExtStr
+        /// Установить данные: последний запуск
         /// </summary>
+        /// <param name="elem">Элемент, в который будет записы данные</param>
+        /// <param name="userName">Имя пользователя</param>
+        /// <param name="dateTime">Дата</param>
+        public void SetStorageData_TimeRunLog(Element elem, string userName, DateTime dateTime)
+        {
+            Schema sch;
+            Entity entity = CheckStorageExists(elem);
+            if (entity == null)
+            {
+                sch = CreateSchema();
+                entity = new Entity(sch);
+            }
+
+            entity.Set<string>(FieldName, $"{userName}: {dateTime}");
+            elem.SetEntity(entity);
+        }
+
+        /// <summary>
+        /// Установить данные: текстовая пометка с разделителем в формате "Id-элемента+Разделитель+Текстовое описание"
+        /// </summary>
+        /// <param name="elem">Элемент, в который будет записы данные, и из которого получаю Id</param>
+        /// <param name="userName">Имя пользователя</param>
+        /// <param name="descr">Текстовое описание</param>
+        public void SetStorageDataWithSeparator_TextLog(Element elem, string userName, string descr)
+        {
+            Schema sch;
+            Entity entity = CheckStorageExists(elem);
+            if (entity == null)
+            {
+                sch = CreateSchema();
+                entity = new Entity(sch);
+            }
+
+            entity.Set<string>(FieldName, $"{elem.Id}{_mainDataSeparator}{userName}: {descr}");
+            elem.SetEntity(entity);
+        }
+
+        /// <summary>
+        /// Очистить данные в ExtensibleStorage
+        /// </summary>
+        /// <param name="elem">Элемент, который будет очищен (string.Empty)</param>
+        public void DropStorageData_TextLog(Element elem)
+        {
+            Entity entity = CheckStorageExists(elem);
+            if (entity != null)
+            {
+                entity.Set<string>(FieldName, string.Empty);
+                elem.SetEntity(entity);
+            }
+        }
+
+        /// <summary>
+        /// Проверить, совпадают ли данные в ExtensibleStorage с ключевой пометкой (например: Id, служебный комментарий и т.п.)
+        /// </summary>
+        /// <param name="elem">Элемент, который нужно проверить</param>
+        /// <param name="checkData">Данные, которые проверяем (ключевая пометка)</param>
+        public bool CheckStorageDataContains_TextLog(Element elem, string checkData)
+        {
+            Schema sch = Schema.Lookup(Guid);
+            if (sch == null) return false;
+
+            Entity entity = CheckStorageExists(elem);
+            if (entity == null) return false;
+            
+            string dataStream = entity.Get<string>(FieldName);
+            if (dataStream == null) return false;
+
+            if (dataStream.Contains(checkData)) return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Проверяет, есть ли хоть какие-то текстовые данные в ExtensibleStorage
+        /// </summary>
+        /// <param name="elem">Элемент проверки</param>
         public bool IsDataExists_Text(Element elem)
         {
             Schema sch = Schema.Lookup(Guid);
@@ -132,6 +177,9 @@ namespace KPLN_Library_ExtensibleStorage
             return false;
         }
 
+        /// <summary>
+        /// Создание Schema для данного экземпляра
+        /// </summary>
         private Schema CreateSchema()
         {
             SchemaBuilder builder = new SchemaBuilder(Guid);
@@ -144,6 +192,10 @@ namespace KPLN_Library_ExtensibleStorage
             return sch;
         }
 
+        /// <summary>
+        /// Проверка наличия ExtensibleStorage для элемента
+        /// </summary>
+        /// <param name="elem">Элемент проверки</param>
         private Entity CheckStorageExists(Element elem)
         {
             Schema sch = Schema.Lookup(Guid);
@@ -155,6 +207,17 @@ namespace KPLN_Library_ExtensibleStorage
                 return ent;
 
             return null;
+        }
+
+        /// <summary>
+        /// Очистка данных в ExtensibleStorage от ключевой пометки (например: Id, служебный комментарий и т.п.), если она есть
+        /// </summary>
+        /// <param name="data">Данные для очистки</param>
+        private string CleanDataByMainSeparator(string data)
+        {
+            string[] splitArray = data.Split(new string[] {_mainDataSeparator}, StringSplitOptions.None);
+            if (splitArray.Length > 1) return splitArray[1];
+            else return splitArray[0];
         }
     }
 }
--- 01_KPLN_Library/KPLN_Library_ExtensibleStorage/KPLN_Library_ExtensibleStorage/ResultMessage.cs
+++ 01_KPLN_Library/KPLN_Library_ExtensibleStorage/KPLN_Library_ExtensibleStorage/ResultMessage.cs
@@ -9,12 +9,18 @@ namespace KPLN_Library_ExtensibleStorage
     }
 
     /// <summary>
-    /// Класс-обертка для сообщений об Extensible Storage
+    /// Класс-обертка для сообщений об ExtensibleStorage
     /// </summary>
     public class ResultMessage
     {
+        /// <summary>
+        /// Описание, которое содержится в ExtensibleStorage БЕЗ технических пометок
+        /// </summary>
         public string Description { get; set; }
 
+        /// <summary>
+        /// Статус полученного ответа
+        /// </summary>
         public MessageStatus CurrentStatus { get; set; }
     }
 }
