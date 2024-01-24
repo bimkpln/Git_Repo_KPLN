337defb0b6405b40eef94db0c4cf14dbbda91a0b
bimManager <bim.kpln@gmail.com>
2024-01-24 14:31:51 +0300

    Revit_Starter 1.0. Плагин для запуска Revit

---
+++ KPLN_Revit_Starter/KPLN_Revit_Starter/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KPLN_Revit_Starter
+{
+    public class Program
+    {
+        [DllImport("USER32.DLL", EntryPoint = "SetForegroundWindow")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        static void Main(string[] args)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo(@"C:\Program Files\Autodesk\Revit 2023\Revit.exe")
+            {
+                CreateNoWindow = false,
+                UseShellExecute = false,
+                WindowStyle = ProcessWindowStyle.Hidden,
+            };
+
+            try
+            {
+                RunRevit(startInfo, args);
+            }
+            catch
+            {
+                // Log error.
+            }
+        }
+
+        static void RunRevit(ProcessStartInfo startInfo, string[] sendKeysPair)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+
+            var process = new Process
+            {
+                StartInfo = startInfo,
+                EnableRaisingEvents = true
+            };
+
+            process.Exited += (sender, args) =>
+            {
+                tcs.SetResult(true);
+                process.Dispose();
+            };
+
+            process.Start();
+            Thread.Sleep(30000);
+
+            IntPtr handle = WindowHandleSearch.GetMainWindow(process.Id);
+            if (SetForegroundWindow(handle))
+            {
+                SendKeys.SendWait(sendKeysPair[0]);
+                Console.WriteLine($"sended to: {handle}");
+            }
+        }
+    }
+}
