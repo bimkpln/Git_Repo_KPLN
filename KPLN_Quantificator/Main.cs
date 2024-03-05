99505903d3c4e389dc77734d9841dcb09e754342
xPRYANIKx <tuleninov.rv@gmail.com>
2024-03-05 10:18:10 +0300

    Quantificator. Сгруппировать коллизии: добавлено автозаполнение поля поиска и горячие клавиши

--- KPLN_Quantificator/Main.cs
+++ KPLN_Quantificator/Main.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Windows.Controls;
 using Autodesk.Navisworks.Api;
+using Autodesk.Navisworks.Api.Clash;
 using Autodesk.Navisworks.Api.Plugins;
 using KPLN_Quantificator.Forms;
 using KPLN_Quantificator.Services;
+using System.Windows.Forms;
+using System.Linq;
+using Autodesk.Navisworks.Api.Interop;
 
 
 namespace KPLN_Quantificator
@@ -14,7 +19,7 @@ namespace KPLN_Quantificator
     [Command("ID_Button_B", DisplayName = "Добавить элементы", Icon = "Source\\update_q_small.png", LargeIcon = "Source\\update_q_big.png", ToolTip = "Добавление элементов (в которые непосредствунно будут добавляться объекты модели) в существующую структуру книги Quantification ", CanToggle = true)]
     [Command("ID_Button_C", DisplayName = "Добавить объекты", Icon = "Source\\create_items_small.png", LargeIcon = "Source\\create_items_big.png", ToolTip = "Наполнение каталогов Quantification объектами модели из выбранных поисковых наборов", CanToggle = true)]
     [Command("ID_Button_D", DisplayName = "Добавить ресурсы", Icon = "Source\\match_resources_small.png", LargeIcon = "Source\\match_resources_big.png", ToolTip = "Сопоставление ресурсов с элементами по выбранному параметру RBS", CanToggle = true)]
-    [Command("ID_Button_E", DisplayName = "Сгруппировать коллизии", Icon = "Source\\group_c_small.png", LargeIcon = "Source\\group_c_big.png", ToolTip = "Группировка коллизий по выбранным параметрам. Сделано на основе «Group Clashes»", CanToggle = true)]
+    [Command("ID_Button_E", DisplayName = "Сгруппировать коллизии", Icon = "Source\\group_c_small.png", LargeIcon = "Source\\group_c_big.png", ToolTip = "Группировка коллизий по выбранным параметрам. Сделано на основе «Group Clashes». Горячие клавиши - Ctrl + G", CanToggle = true)]
     [Command("ID_Button_F", DisplayName = "Подсчет коллизий", Icon = "Source\\counter_small.png", LargeIcon = "Source\\counter_big.png", ToolTip = "Подсчет количества коллизий по разделам (раздел выделяется из имени)", CanToggle = true)]
     [Command("ID_Button_G", DisplayName = "Автоматический комментарий", Icon = "Source\\comment_small.png", LargeIcon = "Source\\comment_big.png", ToolTip = "Создание текстового комментария. Для создания комментария в автоматическом режиме необходимо выделить два эллемента и нажать клавишу Q", CanToggle = true)]
     public class Main : CommandHandlerPlugin
@@ -74,6 +79,11 @@ namespace KPLN_Quantificator
                         case "ID_Button_E":
                             {
                                 ClashGroupsForm clashGroupsForm = new ClashGroupsForm();
+                                try
+                                {
+                                    clashGroupsForm.SearchText.Text = ClashCurrentIssue.CurrentTest?.DisplayName;
+                                }
+                                catch (NullReferenceException) { }
                                 clashGroupsForm.ShowDialog();
                                 GlobalPreferences.state = 0;
                                 
@@ -130,16 +140,29 @@ namespace KPLN_Quantificator_inputPlugin
     [Plugin("KPLN Extention_inputPlugin", "KPLN")]
     public class Main: InputPlugin
     {
-        public override bool KeyDown(Autodesk.Navisworks.Api.View view, KeyModifiers modifier, ushort key, double timeOffset)
+        public override bool KeyUp(Autodesk.Navisworks.Api.View view, KeyModifiers modifier, ushort key, double timeOffset)
         {
             if (Autodesk.Navisworks.Api.Application.ActiveDocument.CurrentSelection.SelectedItems.Count == 2 && key == 81)
             {
-                AddComment.GettingDataForAComment();
-                AddComment.CreateViewpoint();
+                    AddComment.GettingDataForAComment();
+                    AddComment.CreateViewpoint();
+                    return true; 
+            }
+
+            if (modifier == KeyModifiers.Shift && key == 71)
+            {
+                ClashGroupsForm clashGroupsForm = new ClashGroupsForm();
+                try 
+                { 
+                    clashGroupsForm.SearchText.Text = ClashCurrentIssue.CurrentTest?.DisplayName; 
+                }
+                catch (NullReferenceException) {}
+
+                clashGroupsForm.ShowDialog();                      
                 return true;
             }
 
-            return base.KeyDown(view, modifier, key, timeOffset);
+            return base.KeyUp(view, modifier, key, timeOffset);
         }
     }
 }
