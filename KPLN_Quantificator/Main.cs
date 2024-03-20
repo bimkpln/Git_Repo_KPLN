130046bba1b6e46921a20f77b9be8746faa44f18
Roman Tuleninov <tuleninov.rv@gmail.com>
2024-03-20 12:40:45 +0300

    Rtuleninov branch (#2)
    * Quantificator. AddComent 0.1
    
    * Quantificator. Сгруппировать коллизии: добавлено автозаполнение поля поиска и горячие клавиши
    
    * Quantificator. ViewPoint Rename 0.1

--- KPLN_Quantificator/Main.cs
+++ KPLN_Quantificator/Main.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Threading;
-using System.Collections.Generic;
-using System.Windows.Forms;
-using System.Text;
+using System.Windows.Controls;
 using Autodesk.Navisworks.Api;
+using Autodesk.Navisworks.Api.Clash;
 using Autodesk.Navisworks.Api.Plugins;
-using Autodesk.Navisworks.Api.Data;
-using Autodesk.Navisworks.Api.Takeoff;
 using KPLN_Quantificator.Forms;
-using Autodesk.Navisworks.Api.DocumentParts;
 using KPLN_Quantificator.Services;
+using System.Windows.Forms;
+using System.Linq;
+using Autodesk.Navisworks.Api.Interop;
+
 
 namespace KPLN_Quantificator
 {
@@ -20,8 +19,10 @@ namespace KPLN_Quantificator
     [Command("ID_Button_B", DisplayName = "Добавить элементы", Icon = "Source\\update_q_small.png", LargeIcon = "Source\\update_q_big.png", ToolTip = "Добавление элементов (в которые непосредствунно будут добавляться объекты модели) в существующую структуру книги Quantification ", CanToggle = true)]
     [Command("ID_Button_C", DisplayName = "Добавить объекты", Icon = "Source\\create_items_small.png", LargeIcon = "Source\\create_items_big.png", ToolTip = "Наполнение каталогов Quantification объектами модели из выбранных поисковых наборов", CanToggle = true)]
     [Command("ID_Button_D", DisplayName = "Добавить ресурсы", Icon = "Source\\match_resources_small.png", LargeIcon = "Source\\match_resources_big.png", ToolTip = "Сопоставление ресурсов с элементами по выбранному параметру RBS", CanToggle = true)]
-    [Command("ID_Button_E", DisplayName = "Сгруппировать коллизии", Icon = "Source\\group_c_small.png", LargeIcon = "Source\\group_c_big.png", ToolTip = "Группировка коллизий по выбранным параметрам. Сделано на основе «Group Clashes»", CanToggle = true)]
+    [Command("ID_Button_E", DisplayName = "Сгруппировать коллизии", Icon = "Source\\group_c_small.png", LargeIcon = "Source\\group_c_big.png", ToolTip = "Группировка коллизий по выбранным параметрам. Сделано на основе «Group Clashes». Горячие клавиши - Shift + G", CanToggle = true)]
     [Command("ID_Button_F", DisplayName = "Подсчет коллизий", Icon = "Source\\counter_small.png", LargeIcon = "Source\\counter_big.png", ToolTip = "Подсчет количества коллизий по разделам (раздел выделяется из имени)", CanToggle = true)]
+    [Command("ID_Button_G", DisplayName = "Автоматический комментарий", Icon = "Source\\comment_small.png", LargeIcon = "Source\\comment_big.png", ToolTip = "Создание текстового комментария. Для создания комментария в автоматическом режиме необходимо выделить два эллемента и нажать клавишу Q", CanToggle = true)]
+    [Command("ID_Button_H", DisplayName = "Настройка для пакетного переименования точек обзора", Icon = "Source\\rename_small.png", LargeIcon = "Source\\rename_big.png", ToolTip = "Настройка для пакетного переименования точек обзора.\nДля переименования точки обзора - задайте параметры в данном окне, после чего выберите необходимую точку обзора и нажмите клавишу Q", CanToggle = true)]
     public class Main : CommandHandlerPlugin
     {
         public override int ExecuteCommand(string name, params string[] parameters)
@@ -79,6 +80,11 @@ namespace KPLN_Quantificator
                         case "ID_Button_E":
                             {
                                 ClashGroupsForm clashGroupsForm = new ClashGroupsForm();
+                                try
+                                {
+                                    clashGroupsForm.SearchText.Text = ClashCurrentIssue.CurrentTest?.DisplayName;
+                                }
+                                catch (NullReferenceException) { }
                                 clashGroupsForm.ShowDialog();
                                 GlobalPreferences.state = 0;
                                 
@@ -91,6 +97,26 @@ namespace KPLN_Quantificator
                                 ClashesCounter.PrintResult();
                                 GlobalPreferences.state = 0;
 
+                                break;
+                            }
+                        case "ID_Button_G":
+                            {
+                                if (Autodesk.Navisworks.Api.Application.ActiveDocument.CurrentSelection.SelectedItems.Count == 2)
+                                {
+                                    AddComment.GettingDataForAComment();
+                                    AddComment.CreateViewpoint();
+                                }
+                                GlobalPreferences.state = 0;
+                                break;
+                            }
+                        case "ID_Button_H":
+                            {
+                                if (Autodesk.Navisworks.Api.Application.ActiveDocument?.SavedViewpoints?.CurrentSavedViewpoint?.DisplayName != null) 
+                                {
+                                    RenameViewForm renameViewForm = new RenameViewForm();
+                                    renameViewForm.ShowDialog();  
+                                }
+                                GlobalPreferences.state = 0;
                                 break;
                             }
                         default:
@@ -110,10 +136,48 @@ namespace KPLN_Quantificator
                     
                     return 0;
                 }
-            }
-
+            }      
+            
             return 0;
         }
     }
 }
 
+
+namespace KPLN_Quantificator_inputPlugin
+{
+    [Plugin("KPLN Extention_inputPlugin", "KPLN")]
+    public class Main: InputPlugin
+    {
+        public override bool KeyUp(Autodesk.Navisworks.Api.View view, KeyModifiers modifier, ushort key, double timeOffset)
+        {
+            if (modifier == KeyModifiers.Shift && key == 71)
+            {
+                ClashGroupsForm clashGroupsForm = new ClashGroupsForm();
+                try
+                {
+                    clashGroupsForm.SearchText.Text = ClashCurrentIssue.CurrentTest?.DisplayName;
+                }
+                catch (NullReferenceException) { }
+
+                clashGroupsForm.ShowDialog();
+                return true;
+            }
+
+            if (Autodesk.Navisworks.Api.Application.ActiveDocument.CurrentSelection.SelectedItems.Count == 2 && key == 81)
+            {
+                    AddComment.GettingDataForAComment();
+                    AddComment.CreateViewpoint();
+                    return true; 
+            }
+
+            if (Autodesk.Navisworks.Api.Application.ActiveDocument?.SavedViewpoints?.CurrentSavedViewpoint?.DisplayName != null && key == 81)
+            {
+                RenameViewForm renameViewForm = new RenameViewForm();
+                renameViewForm.RenameViewPointObj();
+            }
+
+            return base.KeyUp(view, modifier, key, timeOffset);
+        }
+    }
+}
