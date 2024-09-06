109441c8570cc8e4bd4f14506eac1abf9d83a3a8
xPRYANIKx <tuleninov.rv@gmail.com>
2024-09-06 13:04:44 +0300

    Tools. Объединение двух плагинов в один - "Заполнить параметры на чертежном виде"

--- KPLN_Tools/KPLN_Tools/ExternalCommands/Command_FillInParametersSS.cs
+++ KPLN_Tools/KPLN_Tools/ExternalCommands/Command_FillInParametersSS.cs
@@ -10,7 +10,7 @@ namespace KPLN_Tools.ExternalCommands
     [Transaction(TransactionMode.Manual)]
     [Regeneration(RegenerationOption.Manual)]
 
-    internal class Command_kpPozSum : IExternalCommand
+    internal class Command_FillInParametersSS : IExternalCommand
     {
         public Parameter GetParameterByName(Document doc, Element element, string parameterName)
         {
@@ -31,7 +31,8 @@ namespace KPLN_Tools.ExternalCommands
             UIApplication _uiapp = commandData.Application;
             UIDocument _uidoc = _uiapp.ActiveUIDocument;
             Document _doc = _uidoc.Document;
-           
+
+            // Заполнение параметров: КП_Позиция_Сумма
             IList<Element> allElementsInProject = new FilteredElementCollector(_doc)
             .OfCategory(BuiltInCategory.OST_DetailComponents)
             .WhereElementIsNotElementType()
@@ -67,13 +68,8 @@ namespace KPLN_Tools.ExternalCommands
                     newDict[keyVal] = existing;
                 }
             }
-
-            if (newDict.Count == 0)
-            {
-                MessageBox.Show("Нет параметров удовлетворяющих условиям", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-
-            using (Transaction t = new Transaction(_doc, "KPLN_Заполнение параметров"))
+   
+            using (Transaction t = new Transaction(_doc, "KPLN_Заполнение параметров: КП_Позиция_Сумма"))
             {
                 t.Start();
 
@@ -96,14 +92,74 @@ namespace KPLN_Tools.ExternalCommands
                     }
                 }
 
-                if (newDict.Count != 0)
-                {
-                    MessageBox.Show("Все параметры были заполнены", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);                   
-                }
-
                 t.Commit();
             }
 
+            // Заполнение параметров: КП_И_КолСпецификация
+            List<Element> connectionElements = new List<Element>();
+            FilteredElementCollector collector = new FilteredElementCollector(_doc);
+
+            foreach (Element e in collector.WhereElementIsNotElementType())
+            {
+                if (e.Category != null && e.Category.Name == "Элементы узлов")
+                {
+                    connectionElements.Add(e);
+                }
+            }
+
+            List<Element> elemsMatched = new List<Element>();
+            List<Element> elemsNotMatched = new List<Element>();
+
+            foreach (Element elem in connectionElements)
+            {
+                ElementId typeId = elem.GetTypeId();
+                ElementType elemType = _doc.GetElement(typeId) as ElementType;
+
+                if (elemType != null && (elemType.FamilyName == "076_КШ_Короб перфорированный_(ЭлУзл)" || elemType.FamilyName == "076_КШ_DIN рейка_(ЭлУзл)"))
+                {
+                    elemsMatched.Add(elem);
+                }
+                else
+                {
+                    elemsNotMatched.Add(elem);
+                }
+            }
+
+            using (Transaction t2 = new Transaction(_doc, "KPLN_Заполнение параметров: КП_И_КолСпецификация"))
+            {
+                t2.Start();
+
+                foreach (Element elem in elemsMatched)
+                {
+                    Parameter paramHeight = elem.LookupParameter("КП_Р_Высота");
+
+                    if (paramHeight != null && paramHeight.HasValue)
+                    {
+                        double heightValue = paramHeight.AsDouble() * 304.8;
+                        Parameter paramSpec = elem.LookupParameter("КП_И_КолСпецификация");
+
+                        if (paramSpec != null && !paramSpec.IsReadOnly)
+                        {
+                            paramSpec.Set(heightValue);
+                        }
+                    }
+                }
+
+                foreach (Element elem in elemsNotMatched)
+                {
+                    Parameter paramSpec = elem.LookupParameter("КП_И_КолСпецификация");
+
+                    if (paramSpec != null && !paramSpec.IsReadOnly)
+                    {
+                        paramSpec.Set(1);
+                    }
+                }
+
+                t2.Commit();
+            }
+
+            MessageBox.Show("Все параметры были заполнены", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+      
             return Result.Succeeded;
         }
     }
--- KPLN_Tools/KPLN_Tools/Module.cs
+++ KPLN_Tools/KPLN_Tools/Module.cs
@@ -227,39 +227,24 @@ namespace KPLN_Tools
                     "KPLN_Tools.Imagens.ssSystemsSmall.png",
                     "http://moodle");
 
-                PushButtonData kpPozSum = CreateBtnData(
-                    "Заполнить КП_Позиция_Сумма",
-                    "Заполнить КП_Позиция_Сумма",
-                    "Заполнить КП_Позиция_Сумма",
-                    string.Format("Плагин заполняет параметр КП_Позиция_Сумма для одинаковых семейств на чертежном виде, собирая значения параметров КП_О_Позиция с учетом параметра КП_О_Группирование\n" +
+                PushButtonData ssFillInParameters = CreateBtnData(
+                    "Заполнить параметры на чертежном виде",
+                    "Заполнить параметры на чертежном виде",
+                    "Заполнить параметры на чертежном виде",
+                    string.Format("Плагин заполняет параметр ``КП_Позиция_Сумма`` для одинаковых семейств на чертежном виде, собирая значения параметров ``КП_О_Позиция`` с учетом параметра ``КП_О_Группирование``, " +
+                    "а также заполняет параметр ``КП_И_КолСпецификация`` для семейств категории ``Элементы узлов`` на чертежном виде, у которых в спецификации необходимо учитывать длину, а не количество\n" +
                         "Дата сборки: {0}\nНомер сборки: {1}\nИмя модуля: {2}",
                         ModuleData.Date,
                         ModuleData.Version,
                         ModuleData.ModuleName
                     ),
-                    typeof(ExternalCommands.Command_kpPozSum).FullName,
-                    "KPLN_Tools.Imagens.FillInParamBig.png",
-                    "KPLN_Tools.Imagens.FillInParamSmall.png",
-                    "http://moodle");
-
-                PushButtonData kpKolSpec = CreateBtnData(
-                    "Заполнить КП_И_КолСпецификация",
-                    "Заполнить КП_И_КолСпецификация",
-                    "Заполнить КП_И_КолСпецификация",
-                    string.Format("Плагин заполняет параметр КП_И_КолСпецификация для семейств категории `Элементы узлов` на чертежном виде, у которых в спецификации необходимо учитывать длину, а не количество\n" +
-                        "Дата сборки: {0}\nНомер сборки: {1}\nИмя модуля: {2}",
-                        ModuleData.Date,
-                        ModuleData.Version,
-                        ModuleData.ModuleName
-                    ),
-                    typeof(ExternalCommands.Command_kpKolSpec).FullName,
+                    typeof(ExternalCommands.Command_FillInParametersSS).FullName,
                     "KPLN_Tools.Imagens.FillInParamBig.png",
                     "KPLN_Tools.Imagens.FillInParamSmall.png",
                     "http://moodle");
 
                 ssToolsPullDownBtn.AddPushButton(ssSystems);
-                ssToolsPullDownBtn.AddPushButton(kpPozSum);
-                ssToolsPullDownBtn.AddPushButton(kpKolSpec);
+                ssToolsPullDownBtn.AddPushButton(ssFillInParameters);
             }
             #endregion
 
