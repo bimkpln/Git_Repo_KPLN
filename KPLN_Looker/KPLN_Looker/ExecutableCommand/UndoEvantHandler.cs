4481f62e148728b3cff929b6956bce79aa0bb3c5
BIM_KPLN <bim@kpln.ru>
2024-10-30 12:28:48 +0300

    Looker 1.1.15. Глобальный рефакторинг и добавление блокировки использования буфера обмена

---
+++ KPLN_Looker/KPLN_Looker/ExecutableCommand/UndoEvantHandler.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using KPLN_Loader.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPLN_Looker.ExecutableCommand
+{
+    internal class UndoEvantHandler : IExecutableCommand
+    {
+        public Result Execute(UIApplication app)
+        {
+            try
+            {
+                app.PostCommand(RevitCommandId.LookupPostableCommandId(PostableCommand.Undo));
+            }
+            catch (Autodesk.Revit.Exceptions.InvalidOperationException ioe)
+            {
+                if (!ioe.Message.Contains("Revit does not support more than one command are posted."))
+                    throw ioe;
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
