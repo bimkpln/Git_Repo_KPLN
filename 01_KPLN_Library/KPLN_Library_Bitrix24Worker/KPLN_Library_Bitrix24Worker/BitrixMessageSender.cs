341f06949130ab01475b79d36851a1f61e0dbe9d
BIM_KPLN <bim@kpln.ru>
2025-02-13 18:23:42 +0300

    Мелкие уточнения

--- 01_KPLN_Library/KPLN_Library_Bitrix24Worker/KPLN_Library_Bitrix24Worker/BitrixMessageSender.cs
+++ 01_KPLN_Library/KPLN_Library_Bitrix24Worker/KPLN_Library_Bitrix24Worker/BitrixMessageSender.cs
@@ -3,7 +3,6 @@ using KPLN_Library_SQLiteWorker.FactoryParts;
 using Newtonsoft.Json;
 using System;
 using System.Net.Http;
-using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
