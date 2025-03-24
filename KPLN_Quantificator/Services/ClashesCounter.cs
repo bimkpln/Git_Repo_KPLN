bd56ca423bc82616cee0a90b1f6959a75ca9b563
xPRYANIKx <tuleninov.rv@gmail.com>
2025-03-24 13:38:58 +0300

    KPLN_Quantificator. Добавлен подсчёт групп в инструмент "Подсчет коллизий"

--- KPLN_Quantificator/Services/ClashesCounter.cs
+++ KPLN_Quantificator/Services/ClashesCounter.cs
@@ -18,19 +18,23 @@ namespace KPLN_Quantificator.Services
             "ИТП",
             "ВК",
             "ПТ",
+            "ЭОМ",
             "СС",
             "АВ",
-            "ССАВ",
-            "ЭОМ"
+            "ССАВ",      
         };
 
         private static Dictionary<string, int> _counterResult = new Dictionary<string, int>();
+        private static Dictionary<string, int> _counterGroups = new Dictionary<string, int>();
+        private static Dictionary<string, int> _counterNonGroups = new Dictionary<string, int>(); 
 
         private static List<string> _errorTestNames = new List<string>();
 
         public static void Prepare()
         {
             _counterResult.Clear();
+            _counterGroups.Clear();
+            _counterNonGroups.Clear();
             _errorTestNames.Clear();
         }
 
@@ -40,38 +44,63 @@ namespace KPLN_Quantificator.Services
         public static void Execute()
         {
             DocumentClashTests docClashTests = _doc.GetClash().TestsData;
+
             foreach (SavedItem savedItem in docClashTests.Tests)
             {
                 bool isExsist = false;
+                bool isSSAV = savedItem.DisplayName.Contains("СС") && savedItem.DisplayName.Contains("АВ");
+
                 foreach (string namePart in _testNames)
                 {
                     if (savedItem.DisplayName.Contains(namePart))
                     {
+                        if (namePart == "СС" && isSSAV)
+                            continue;
+
                         isExsist = true;
 
                         int counter = 0;
+                        int groupCounter = 0;
+                        int nonGroupCounter = 0;
+
                         ClashTest currentClashTest = savedItem as ClashTest;
+
                         if (currentClashTest != null)
                         {
                             SavedItemCollection savedItemsColl = currentClashTest.Children;
+
                             foreach (SavedItem savedItemInClashTest in savedItemsColl)
                             {
                                 ClashResult clashResult = savedItemInClashTest as ClashResult;
+
                                 if (clashResult != null)
                                 {
                                     if (clashResult.Status == ClashResultStatus.Active || clashResult.Status == ClashResultStatus.New)
                                     {
+                                        if (savedItemInClashTest.IsGroup)
+                                        {
+                                            groupCounter++; 
+                                        }
+                                        else
+                                        {
+                                            nonGroupCounter++; 
+                                        }
+
                                         counter++;
                                     }
                                 }
                                 else if (savedItemInClashTest.IsGroup)
                                 {
                                     GroupItem groupItem = savedItemInClashTest as GroupItem;
+
                                     if (groupItem != null)
                                     {
+                                        groupCounter++;
+
                                         foreach (SavedItem savedItemInClashTestInGroup in groupItem.Children)
                                         {
                                             ClashResult clashResultInGroup = savedItemInClashTestInGroup as ClashResult;
+
                                             if (clashResultInGroup != null)
                                             {
                                                 if (clashResultInGroup.Status == ClashResultStatus.Active || clashResultInGroup.Status == ClashResultStatus.New)
@@ -84,12 +113,34 @@ namespace KPLN_Quantificator.Services
                                         }
                                     }
                                 }
+                                else 
+                                {
+                                    ClashResult clashResultNonGroup = savedItemInClashTest as ClashResult;
+                                    if (clashResultNonGroup != null)
+                                    {
+                                        if (clashResultNonGroup.Status == ClashResultStatus.Active || clashResultNonGroup.Status == ClashResultStatus.New)
+                                        {
+                                            nonGroupCounter++;
+                                        }
+                                    }
+                                }
 
                             }
+
                             if (_counterResult.ContainsKey(namePart))
                                 _counterResult[namePart] += counter;
                             else
                                 _counterResult.Add(namePart, counter);
+
+                            if (_counterGroups.ContainsKey(namePart))
+                                _counterGroups[namePart] += groupCounter;
+                            else
+                                _counterGroups.Add(namePart, groupCounter);
+
+                            if (_counterNonGroups.ContainsKey(namePart))
+                                _counterNonGroups[namePart] += nonGroupCounter;
+                            else
+                                _counterNonGroups.Add(namePart, nonGroupCounter);
                         }
                         else
                             Output.PrintAlert($"Проблемы при определении отчета {savedItem.DisplayName}");
@@ -108,9 +159,16 @@ namespace KPLN_Quantificator.Services
         {
             if (_counterResult.Count > 0)
             {
-                foreach (KeyValuePair<string, int> kvp in _counterResult)
+                foreach (string key in _testNames)
                 {
-                    Output.PrintSuccess($"Количество коллизий для раздела {kvp.Key} составляет: {kvp.Value} шт.");
+                    if (_counterResult.ContainsKey(key))
+                    {
+                        int clashCount = _counterResult[key];
+                        int groupCount = _counterGroups.ContainsKey(key) ? _counterGroups[key] : 0;
+                        int nonGroupCount = _counterNonGroups.ContainsKey(key) ? _counterNonGroups[key] : 0;  // Получаем количество вне групп
+
+                        Output.PrintSuccess($"Количество коллизий для раздела {key} составляет: {clashCount} шт. (Групп: {groupCount + nonGroupCount})");
+                    }
                 }
             }
 
