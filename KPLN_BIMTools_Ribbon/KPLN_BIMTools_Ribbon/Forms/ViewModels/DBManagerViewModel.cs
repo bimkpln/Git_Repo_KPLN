4346b2fdeab7dbd0a529502410e9ff6ec27c24f2
BIM_KPLN <bim@kpln.ru>
2025-07-23 12:35:08 +0300

    Добавлен отлов ошибок при создании проектов в БД

--- KPLN_BIMTools_Ribbon/KPLN_BIMTools_Ribbon/Forms/ViewModels/DBManagerViewModel.cs
+++ KPLN_BIMTools_Ribbon/KPLN_BIMTools_Ribbon/Forms/ViewModels/DBManagerViewModel.cs
@@ -58,86 +58,26 @@ namespace KPLN_BIMTools_Ribbon.Forms.ViewModels
 
         private void CreateDBProject()
         {
-            // Предварительная замена части пути к папке на серверное имя (так преобразуется ModelPath)
-            string replacedServerPath = string.Empty;
-            if (DBPrjWrapper.WrServerPath.StartsWith("Y:\\"))
-                replacedServerPath = DBPrjWrapper.WrServerPath.Replace("Y:\\", "\\\\stinproject.local\\project\\");
-            else if (DBPrjWrapper.WrServerPath.StartsWith("Z:\\"))
-                replacedServerPath = DBPrjWrapper.WrServerPath.Replace("Y:\\", "\\\\fs01\\lib\\");
-
-            #region Верификация
-            // Проверка на пустые значения
-            if (string.IsNullOrEmpty(DBPrjWrapper.WrName)
-                || (string.IsNullOrEmpty(DBPrjWrapper.WrCode) || DBPrjWrapper.WrCode.Any(c => char.IsLower(c)) || !DBPrjWrapper.WrCode.All(c => char.IsLetterOrDigit(c)))
-                || string.IsNullOrEmpty(DBPrjWrapper.WrStage)
-                || (DBPrjWrapper.WrRevitVersion != 2020 && DBPrjWrapper.WrRevitVersion != 2023)
-                || string.IsNullOrEmpty(DBPrjWrapper.WrServerPath))
+            try
             {
-                MessageBox.Show(
-                    "Для создания проекта как минимум нужно указать:\n" +
-                        "Имя проекта;\nКод проекта (только заглавные и цифры);\nСтадию проекта;\nВерсию Revit (2020 или 2023);\nПуть к папке стадии на сервере",
-                    "KPLN_DB: Ошибка!",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Error);
+                // Предварительная замена части пути к папке на серверное имя (так преобразуется ModelPath)
+                string replacedServerPath = string.Empty;
+                if (DBPrjWrapper.WrServerPath.StartsWith("Y:\\"))
+                    replacedServerPath = DBPrjWrapper.WrServerPath.Replace("Y:\\", "\\\\stinproject.local\\project\\");
+                else if (DBPrjWrapper.WrServerPath.StartsWith("Z:\\"))
+                    replacedServerPath = DBPrjWrapper.WrServerPath.Replace("Y:\\", "\\\\fs01\\lib\\");
 
-                return;
-            }
-
-
-            // Проверка на эквивалентные проекты в БД
-            IEnumerable<DBProject> dbrjColl = DBMainService
-                .ProjectDbService
-                .GetDBProjects_ByRVersion(DBPrjWrapper.WrRevitVersion);
-
-            IEnumerable<DBProject> dbrjColl_EqualCodeANDStage = dbrjColl
-                .Where(prj => prj.Code.Equals(DBPrjWrapper.WrCode) && prj.Stage.Equals(DBPrjWrapper.WrStage));
-            if (dbrjColl_EqualCodeANDStage.Any())
-            {
-                MessageBox.Show(
-                    $"Проект с кодом {DBPrjWrapper.WrCode} и стадией {DBPrjWrapper.WrStage} - уже существует",
-                    "KPLN_DB: Ошибка!",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Error);
-
-                return;
-            }
-
-            IEnumerable<DBProject> dbrjColl_EqualPath = dbrjColl
-                .Where(prj => prj.MainPath.Contains(replacedServerPath)
-                    || (!string.IsNullOrEmpty(DBPrjWrapper.WrRevitServerPath) && prj.RevitServerPath.Contains(DBPrjWrapper.WrRevitServerPath))
-                    || (!string.IsNullOrEmpty(DBPrjWrapper.WrRevitServerPath2) && prj.RevitServerPath.Contains(DBPrjWrapper.WrRevitServerPath2))
-                    || (!string.IsNullOrEmpty(DBPrjWrapper.WrRevitServerPath3) && prj.RevitServerPath.Contains(DBPrjWrapper.WrRevitServerPath3))
-                    || (!string.IsNullOrEmpty(DBPrjWrapper.WrRevitServerPath4) && prj.RevitServerPath.Contains(DBPrjWrapper.WrRevitServerPath4)));
-            if (dbrjColl_EqualPath.Any())
-            {
-                MessageBox.Show(
-                    $"Проект по указанному пути - уже существует",
-                    "KPLN_DB: Ошибка!",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Error);
-
-                return;
-            }
-
-            // Проверка существавания путей на серверах
-            if (!Directory.Exists(DBPrjWrapper.WrServerPath))
-            {
-                MessageBox.Show(
-                    "Указанной папки не существует",
-                    "KPLN_DB: Ошибка!",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Error);
-
-                return;
-            }
-            // Проверка что это папка стадии
-            else
-            {
-                string[] parts = DBPrjWrapper.WrServerPath.Split('\\');
-                if (parts.Length == 0)
+                #region Верификация
+                // Проверка на пустые значения
+                if (string.IsNullOrEmpty(DBPrjWrapper.WrName)
+                    || (string.IsNullOrEmpty(DBPrjWrapper.WrCode) || DBPrjWrapper.WrCode.Any(c => char.IsLower(c)) || !DBPrjWrapper.WrCode.All(c => char.IsLetterOrDigit(c)))
+                    || string.IsNullOrEmpty(DBPrjWrapper.WrStage)
+                    || (DBPrjWrapper.WrRevitVersion != 2020 && DBPrjWrapper.WrRevitVersion != 2023)
+                    || string.IsNullOrEmpty(DBPrjWrapper.WrServerPath))
                 {
                     MessageBox.Show(
-                        "Невозможно проанализировать путь. Нужно использовать разделитеть '\'",
+                        "Для создания проекта как минимум нужно указать:\n" +
+                            "Имя проекта;\nКод проекта (только заглавные и цифры);\nСтадию проекта;\nВерсию Revit (2020 или 2023);\nПуть к папке стадии на сервере",
                         "KPLN_DB: Ошибка!",
                         MessageBoxButton.OK,
                         MessageBoxImage.Error);
@@ -145,54 +85,126 @@ namespace KPLN_BIMTools_Ribbon.Forms.ViewModels
                     return;
                 }
 
-                string lastPart = parts[parts.Length - 1];
-                if (!lastPart.ToLower().Contains("стадия") && !lastPart.ToLower().Contains("концепция") && !lastPart.ToLower().Contains("агр") && !lastPart.ToLower().Contains("аго"))
+
+                // Проверка на эквивалентные проекты в БД
+                IEnumerable<DBProject> dbrjColl = DBMainService
+                    .ProjectDbService
+                    .GetDBProjects_ByRVersion(DBPrjWrapper.WrRevitVersion);
+
+                IEnumerable<DBProject> dbrjColl_EqualCodeANDStage = dbrjColl
+                    .Where(prj => prj.Code.Equals(DBPrjWrapper.WrCode) && prj.Stage.Equals(DBPrjWrapper.WrStage));
+                if (dbrjColl_EqualCodeANDStage.Any())
                 {
                     MessageBox.Show(
-                        "Нужно указывать корневую папку стадии (имя должно содержать 'Концепция', или 'АГР', или 'АГО', или 'Cтадия')",
+                        $"Проект с кодом {DBPrjWrapper.WrCode} и стадией {DBPrjWrapper.WrStage} - уже существует",
                         "KPLN_DB: Ошибка!",
                         MessageBoxButton.OK,
                         MessageBoxImage.Error);
 
                     return;
                 }
+
+                IEnumerable<DBProject> dbrjColl_EqualPath = dbrjColl
+                    .Where(prj => prj.MainPath.Contains(replacedServerPath)
+                        || (!string.IsNullOrEmpty(DBPrjWrapper.WrRevitServerPath) && prj.RevitServerPath.Contains(DBPrjWrapper.WrRevitServerPath))
+                        || (!string.IsNullOrEmpty(DBPrjWrapper.WrRevitServerPath2) && prj.RevitServerPath.Contains(DBPrjWrapper.WrRevitServerPath2))
+                        || (!string.IsNullOrEmpty(DBPrjWrapper.WrRevitServerPath3) && prj.RevitServerPath.Contains(DBPrjWrapper.WrRevitServerPath3))
+                        || (!string.IsNullOrEmpty(DBPrjWrapper.WrRevitServerPath4) && prj.RevitServerPath.Contains(DBPrjWrapper.WrRevitServerPath4)));
+                if (dbrjColl_EqualPath.Any())
+                {
+                    MessageBox.Show(
+                        $"Проект по указанному пути - уже существует",
+                        "KPLN_DB: Ошибка!",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+
+                    return;
+                }
+
+                // Проверка существавания путей на серверах
+                if (!Directory.Exists(DBPrjWrapper.WrServerPath))
+                {
+                    MessageBox.Show(
+                        "Указанной папки не существует",
+                        "KPLN_DB: Ошибка!",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+
+                    return;
+                }
+                // Проверка что это папка стадии
+                else
+                {
+                    string[] parts = DBPrjWrapper.WrServerPath.Split('\\');
+                    if (parts.Length == 0)
+                    {
+                        MessageBox.Show(
+                            "Невозможно проанализировать путь. Нужно использовать разделитеть '\'",
+                            "KPLN_DB: Ошибка!",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error);
+
+                        return;
+                    }
+
+                    string lastPart = parts[parts.Length - 1];
+                    if (!lastPart.ToLower().Contains("стадия") && !lastPart.ToLower().Contains("концепция") && !lastPart.ToLower().Contains("агр") && !lastPart.ToLower().Contains("аго"))
+                    {
+                        MessageBox.Show(
+                            "Нужно указывать корневую папку стадии (имя должно содержать 'Концепция', или 'АГР', или 'АГО', или 'Cтадия')",
+                            "KPLN_DB: Ошибка!",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error);
+
+                        return;
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(DBPrjWrapper.WrRevitServerPath) && RSPathError(DBPrjWrapper.WrRevitServerPath))
+                    return;
+
+                if (!string.IsNullOrEmpty(DBPrjWrapper.WrRevitServerPath2) && RSPathError(DBPrjWrapper.WrRevitServerPath2))
+                    return;
+
+                if (!string.IsNullOrEmpty(DBPrjWrapper.WrRevitServerPath3) && RSPathError(DBPrjWrapper.WrRevitServerPath3))
+                    return;
+
+                if (!string.IsNullOrEmpty(DBPrjWrapper.WrRevitServerPath4) && RSPathError(DBPrjWrapper.WrRevitServerPath4))
+                    return;
+                #endregion
+
+                // Создание
+                Task<int> createTask = DBMainService.ProjectDbService.CreateDBDocument(new DBProject
+                {
+                    Name = DBPrjWrapper.WrName,
+                    Code = DBPrjWrapper.WrCode,
+                    Stage = DBPrjWrapper.WrStage,
+                    RevitVersion = DBPrjWrapper.WrRevitVersion,
+                    MainPath = replacedServerPath,
+                    RevitServerPath = DBPrjWrapper.WrRevitServerPath,
+                    RevitServerPath2 = DBPrjWrapper.WrRevitServerPath2,
+                    RevitServerPath3 = DBPrjWrapper.WrRevitServerPath3,
+                    RevitServerPath4 = DBPrjWrapper.WrRevitServerPath4,
+                });
+                createTask.Wait();
+
+                if (createTask.Result > 0)
+                {
+                    MessageBox.Show(
+                        "Проект успешно создан!",
+                        "KPLN_DB: Создание проекта",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Asterisk);
+                }
             }
-
-            if (!string.IsNullOrEmpty(DBPrjWrapper.WrRevitServerPath) && RSPathError(DBPrjWrapper.WrRevitServerPath))
-                return;
-
-            if (!string.IsNullOrEmpty(DBPrjWrapper.WrRevitServerPath2) && RSPathError(DBPrjWrapper.WrRevitServerPath2))
-                return;
-
-            if (!string.IsNullOrEmpty(DBPrjWrapper.WrRevitServerPath3) && RSPathError(DBPrjWrapper.WrRevitServerPath3))
-                return;
-
-            if (!string.IsNullOrEmpty(DBPrjWrapper.WrRevitServerPath4) && RSPathError(DBPrjWrapper.WrRevitServerPath4))
-                return;
-            #endregion
-
-            // Создание
-            Task<int> createTask = DBMainService.ProjectDbService.CreateDBDocument(new DBProject
-            {
-                Name = DBPrjWrapper.WrName,
-                Code = DBPrjWrapper.WrCode,
-                Stage = DBPrjWrapper.WrStage,
-                RevitVersion = DBPrjWrapper.WrRevitVersion,
-                MainPath = replacedServerPath,
-                RevitServerPath = DBPrjWrapper.WrRevitServerPath,
-                RevitServerPath2 = DBPrjWrapper.WrRevitServerPath2,
-                RevitServerPath3 = DBPrjWrapper.WrRevitServerPath3,
-                RevitServerPath4 = DBPrjWrapper.WrRevitServerPath4,
-            });
-            createTask.Wait();
-
-            if (createTask.Result > 0)
+            catch (System.Exception ex)
             {
                 MessageBox.Show(
-                    "Проект успешно создан!",
-                    "KPLN_DB: Создание проекта",
+                    $"При создании возникла ошибка. Для начала - убедись, что минимальный набор полей заполнен, и данные в них прошли верификацию (не горят красным)." +
+                        $"\n\nЕсли всё с твоей стороны хорошо - отправь ошбику разработчику:\n{ex.Message}",
+                    "KPLN_DB: Ошибка!",
                     MessageBoxButton.OK,
-                    MessageBoxImage.Asterisk);
+                    MessageBoxImage.Error);
             }
         }
 
