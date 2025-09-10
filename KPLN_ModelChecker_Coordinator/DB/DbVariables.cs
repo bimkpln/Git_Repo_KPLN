cfb456c37e8ec2378a6593042ae6a8f6959094da
BIM_KPLN <bim@kpln.ru>
2025-09-10 17:56:49 +0300

    Удалена старая версия плагина (на его место добавиться новая, с другой логикой)

--- KPLN_ModelChecker_Coordinator/DB/DbVariables.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KPLN_ModelChecker_Coordinator.DB
-{
-    public static class DbVariables
-    {
-        public static readonly string Separator_Element = "~SE00~";
-        public static readonly string Separator_SubElement = "~SE01~";
-    }
-}
