33a959ae413f0bbafaec117d48cdacbbfd7291f7
BIM_KPLN <bim@kpln.ru>
2025-10-13 11:37:51 +0300

    1.11.8. Уточнён плагин по металоёмкости

--- KPLN_Tools/KPLN_Tools/ExternalCommands/Command_KR_SMNX_RebarHelper.cs
+++ KPLN_Tools/KPLN_Tools/ExternalCommands/Command_KR_SMNX_RebarHelper.cs
@@ -11,6 +11,133 @@ using System.Windows.Forms;
 
 namespace KPLN_Tools.ExternalCommands
 {
+    internal sealed class KGConstruction 
+    {
+        private Solid[] _constrSolid;
+
+        public KGConstruction(Element elem, string code, double volume)
+        {
+            ConstrElem = elem;
+            ConstrCode = code;
+            ConstrVolume = volume;
+        }
+
+        internal Element ConstrElem { get; }
+
+        internal string ConstrCode { get; }
+
+        internal double ConstrVolume { get; }
+
+        internal Solid[] ConstrSolidColl 
+        {
+            get
+            {
+                if (_constrSolid == null)
+                    _constrSolid = GeomUtil.GetElementSolids(ConstrElem);
+
+                return _constrSolid;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Работа с геомтерией
+    /// </summary>
+    internal static class GeomUtil
+    {
+        internal static Solid[] GetElementSolids(Element element)
+        {
+            List<Solid> solidColl = new List<Solid>();
+
+            Options opt = new Options
+            {
+                DetailLevel = ViewDetailLevel.Fine,
+                ComputeReferences = true
+            };
+            GeometryElement geomElem = element.get_Geometry(opt);
+            if (geomElem != null)
+            {
+                GetSolidsFromGeomElem(geomElem, Transform.Identity, solidColl);
+            }
+
+            BoundingBoxXYZ elemBbox = element.get_BoundingBox(null);
+            Solid[] solidWithVolume = solidColl.Where(s => s.Volume > 0).ToArray();
+            if (solidWithVolume.Length > 5 && elemBbox != null)
+                return new Solid[1] { CreateSolidFromBoundingBox(elemBbox) };
+
+            return solidWithVolume;
+        }
+
+        /// <summary>
+        /// Получить Solid из элемента
+        /// </summary>
+        private static void GetSolidsFromGeomElem(GeometryElement geometryElement, Transform transformation, IList<Solid> solids)
+        {
+            foreach (GeometryObject geomObject in geometryElement)
+            {
+                switch (geomObject)
+                {
+                    case Solid solid:
+                        solids.Add(solid);
+                        break;
+
+                    case GeometryInstance geomInstance:
+                        GetSolidsFromGeomElem(geomInstance.GetInstanceGeometry(), geomInstance.Transform.Multiply(transformation), solids);
+                        break;
+
+                    case GeometryElement geomElem:
+                        GetSolidsFromGeomElem(geomElem, transformation, solids);
+                        break;
+                }
+            }
+        }
+
+        private static Solid CreateSolidFromBoundingBox(BoundingBoxXYZ bbox)
+        {
+            // Мінімальныя і максімальныя каардынаты ў bbox
+            XYZ min = bbox.Min;
+            XYZ max = bbox.Max;
+
+            // Улічваем трансфармацыю BoundingBox
+            Transform t = bbox.Transform;
+
+            // Ствараем 8 кропак паралелепіпеда
+            XYZ p0 = t.OfPoint(new XYZ(min.X, min.Y, min.Z));
+            XYZ p1 = t.OfPoint(new XYZ(max.X, min.Y, min.Z));
+            XYZ p2 = t.OfPoint(new XYZ(max.X, max.Y, min.Z));
+            XYZ p3 = t.OfPoint(new XYZ(min.X, max.Y, min.Z));
+
+            XYZ p4 = t.OfPoint(new XYZ(min.X, min.Y, max.Z));
+            XYZ p5 = t.OfPoint(new XYZ(max.X, min.Y, max.Z));
+            XYZ p6 = t.OfPoint(new XYZ(max.X, max.Y, max.Z));
+            XYZ p7 = t.OfPoint(new XYZ(min.X, max.Y, max.Z));
+
+            // Ніжняя грань
+            List<Curve> baseLoop = new List<Curve>()
+            {
+                Line.CreateBound(p0, p1),
+                Line.CreateBound(p1, p2),
+                Line.CreateBound(p2, p3),
+                Line.CreateBound(p3, p0)
+            };
+
+            CurveLoop curveLoop = CurveLoop.Create(baseLoop);
+
+            // Напрамак экструзіі (па Z)
+            XYZ extrusionDir = (p4 - p0).Normalize();
+            double height = (p4 - p0).GetLength();
+
+            // Ствараем solid праз экструзію
+            Solid solid = GeometryCreationUtilities.CreateExtrusionGeometry(
+                new List<CurveLoop> { curveLoop },
+                extrusionDir,
+                height);
+
+            return solid;
+        }
+    }
+
+
     [Transaction(TransactionMode.Manual)]
     [Regeneration(RegenerationOption.Manual)]
     internal class Command_KR_SMNX_RebarHelper : IExternalCommand
@@ -30,10 +157,8 @@ namespace KPLN_Tools.ExternalCommands
             "220_",
             "221_",
             "225_",
-            "230_",
             "232_",
             "233_",
-            "250_",
             "263_",
             "264_",
             "265_",
@@ -56,13 +181,10 @@ namespace KPLN_Tools.ExternalCommands
             "353_",
         };
 
-        private Dictionary<Element, List<Element>> _hostForRebarsDict = new Dictionary<Element, List<Element>>();
-
-        private Dictionary<string, List<Element>> _fatalErrorConstrElemsDict = new Dictionary<string, List<Element>>();
-        
-        private Dictionary<string, List<Element>> _errorConstrElemsDict = new Dictionary<string, List<Element>>();
-
-        private Dictionary<string, double> _constrsDB = new Dictionary<string, double>();
+        private readonly List<KGConstruction> _kgConstrColl= new List<KGConstruction>();
+        private readonly Dictionary<ElementId, List<Element>> _hostForRebarsDict = new Dictionary<ElementId, List<Element>>();
+        private readonly Dictionary<string, List<Element>> _fatalErrorConstrElemsDict = new Dictionary<string, List<Element>>();
+        private readonly Dictionary<string, List<Element>> _errorConstrElemsDict = new Dictionary<string, List<Element>>();
 
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
@@ -176,7 +298,7 @@ namespace KPLN_Tools.ExternalCommands
                         else
                             constrCode = $"{mark}~{volume}~{constr.Id}";
 
-                        _constrsDB[constrCode] = volume;
+                        _kgConstrColl.Add(new KGConstruction(constr, constrCode, volume));
                     }
                 }
 
@@ -189,6 +311,7 @@ namespace KPLN_Tools.ExternalCommands
 
                 foreach (Element rebar in rebars)
                 {
+                    int index = rebars.IndexOf(rebar);
                     PrepareHostForRebars(doc, rebar);
                 }
 
@@ -196,7 +319,7 @@ namespace KPLN_Tools.ExternalCommands
                 {
                     t.Start("SMNX: Зпись данных в арматуру");
 
-                    SetParamData();
+                    SetParamData(doc);
 
                     if (_fatalErrorConstrElemsDict.Count > 0)
                     {
@@ -251,7 +374,7 @@ namespace KPLN_Tools.ExternalCommands
             }
 
             int isIfc = isIfcRebarParam.AsInteger();
-            Parameter hostNameParam = null;
+            Parameter hostNameParam;
             Element hostElement = null;
             if (isIfc == 1)
                 hostNameParam = rebarElement.LookupParameter(_constrRebarMark);
@@ -293,78 +416,74 @@ namespace KPLN_Tools.ExternalCommands
                 }
 
                 string hostMark = hostNameParam.AsString();
+                if (string.IsNullOrEmpty(hostMark)) 
+                    return;
+
+                // Обнуляю хост если это уровень
+                if (hostElement != null && hostElement is Level _)
+                    hostElement = null;
+                
                 double tempIntersectSolidVolume = 0;
                 // Попытка поиска основания по геометрии
                 if (hostElement == null)
                 {
-                    List<Solid> rebarSolids = GetElementSolids(rebarElement);
+                    Solid[] rebarSolids = GeomUtil.GetElementSolids(rebarElement);
                     // Игнор родительских пустых семейств
-                    if (rebarSolids.All(s => s.Volume == 0)) return;
+                    if (!rebarSolids.Any()) return;
 
-                    foreach (KeyValuePair<string, double> kvp in _constrsDB)
+                    foreach (KGConstruction kgConstr in _kgConstrColl)
                     {
-                        if (kvp.Key.Split('~')[0].StartsWith(hostMark))
+                        if (!kgConstr.ConstrCode.Split('~')[0].StartsWith(hostMark)) continue;
+                        
+                        isErorMark = false;
+                         
+                        // Игнор пустых основ
+                        if (!kgConstr.ConstrSolidColl.Any()) return;
+
+                        try
                         {
-                            isErorMark = false;
-                            ElementId elemId = new ElementId(int.Parse(kvp.Key.Split('~')[2]));
-                            Element elem = doc.GetElement(elemId);
-                            if (elem != null)
+                            // Попытка поиска основания по Solid (точное положение)
+                            foreach (Solid rebarSolid in rebarSolids)
                             {
-                                List<Solid> elemSolids = GetElementSolids(elem);
-                                // Игнор пустых основ
-                                if (elemSolids.All(s => s.Volume == 0)) return;
-
-                                try
+                                foreach (Solid elemSolid in kgConstr.ConstrSolidColl)
                                 {
-                                    // Попытка поиска основания по Solid (точное положение)
-                                    foreach (Solid rebarSolid in rebarSolids)
+                                    Solid intersectionSolid = BooleanOperationsUtils.ExecuteBooleanOperation(rebarSolid, elemSolid, BooleanOperationsType.Intersect);
+                                    if (intersectionSolid != null && intersectionSolid.Volume > tempIntersectSolidVolume)
                                     {
-                                        foreach (Solid elemSolid in elemSolids)
-                                        {
-                                            Solid intersectionSolid = BooleanOperationsUtils.ExecuteBooleanOperation(rebarSolid, elemSolid, BooleanOperationsType.Intersect);
-                                            if (intersectionSolid != null && intersectionSolid.Volume > tempIntersectSolidVolume)
-                                            {
-                                                tempIntersectSolidVolume = intersectionSolid.Volume;
-                                                hostElement = elem;
-                                            }
-                                        }
+                                        tempIntersectSolidVolume = intersectionSolid.Volume;
+                                        hostElement = kgConstr.ConstrElem;
                                     }
+                                }
+                            }
 
-                                    // Попытка поиска основания по притягиванию по Z (пониженная точность)
-                                    if (hostElement == null)
+                            // Попытка поиска основания по притягиванию по Z (пониженная точность)
+                            if (hostElement == null)
+                            {
+                                foreach (Solid rebarSolid in rebarSolids)
+                                {
+                                    Transform rebarTransform = rebarSolid.GetBoundingBox().Transform;
+                                    Transform rebarInverseTransform = rebarTransform.Inverse;
+                                    Solid rebarZerotransformSolid = SolidUtils.CreateTransformed(rebarSolid, rebarInverseTransform);
+                                    foreach (Solid elemSolid in kgConstr.ConstrSolidColl)
                                     {
-                                        foreach (Solid rebarSolid in rebarSolids)
-                                        {
-                                            Transform rebarTransform = rebarSolid.GetBoundingBox().Transform;
-                                            Transform rebarInverseTransform = rebarTransform.Inverse;
-                                            Solid rebarZerotransformSolid = SolidUtils.CreateTransformed(rebarSolid, rebarInverseTransform);
-                                            foreach (Solid elemSolid in elemSolids)
-                                            {
-                                                XYZ elemCentroid = elemSolid.ComputeCentroid();
-                                                rebarTransform.Origin = new XYZ(rebarTransform.Origin.X, rebarTransform.Origin.Y, elemCentroid.Z);
+                                        XYZ elemCentroid = elemSolid.ComputeCentroid();
+                                        rebarTransform.Origin = new XYZ(rebarTransform.Origin.X, rebarTransform.Origin.Y, elemCentroid.Z);
 
-                                                Solid transformedByElemRebarSolid = SolidUtils.CreateTransformed(rebarZerotransformSolid, rebarTransform);
-                                                Solid intersectionSolid = BooleanOperationsUtils.ExecuteBooleanOperation(elemSolid, transformedByElemRebarSolid, BooleanOperationsType.Intersect);
-                                                if (intersectionSolid != null && intersectionSolid.Volume > tempIntersectSolidVolume)
-                                                {
-                                                    tempIntersectSolidVolume = intersectionSolid.Volume;
-                                                    hostElement = elem;
-                                                }
-                                            }
+                                        Solid transformedByElemRebarSolid = SolidUtils.CreateTransformed(rebarZerotransformSolid, rebarTransform);
+                                        Solid intersectionSolid = BooleanOperationsUtils.ExecuteBooleanOperation(elemSolid, transformedByElemRebarSolid, BooleanOperationsType.Intersect);
+                                        if (intersectionSolid != null && intersectionSolid.Volume > tempIntersectSolidVolume)
+                                        {
+                                            tempIntersectSolidVolume = intersectionSolid.Volume;
+                                            hostElement = kgConstr.ConstrElem;
                                         }
                                     }
                                 }
-                                //Отлов ошибки для сложной геометрии, для которой невозможно выполнить анализ на коллизии (нужно перемоделить элемент, что не приемлемо)
-                                catch (Autodesk.Revit.Exceptions.InvalidOperationException)
-                                {
-                                    continue;
-                                }
                             }
-                            else
-                            {
-                                TaskDialog.Show("Ошибка", $"Не удалось найти основу с id: {elemId} для арматуры с id {rebarElement.Id}. Покажи разработчику");
-                                throw new Exception();
-                            }
+                        }
+                        //Отлов ошибки для сложной геометрии, для которой невозможно выполнить анализ на коллизии (нужно перемоделить элемент, что не приемлемо)
+                        catch (Autodesk.Revit.Exceptions.InvalidOperationException)
+                        {
+                            continue;
                         }
                     }
                 }
@@ -392,10 +511,10 @@ namespace KPLN_Tools.ExternalCommands
 
                 if (hostElement != null)
                 {
-                    if (_hostForRebarsDict.ContainsKey(hostElement))
-                        _hostForRebarsDict[hostElement].Add(rebarElement);
+                    if (_hostForRebarsDict.ContainsKey(hostElement.Id))
+                        _hostForRebarsDict[hostElement.Id].Add(rebarElement);
                     else
-                        _hostForRebarsDict.Add(hostElement, new List<Element>() { rebarElement });
+                        _hostForRebarsDict.Add(hostElement.Id, new List<Element>() { rebarElement });
                 }
                 else
                 {
@@ -408,56 +527,20 @@ namespace KPLN_Tools.ExternalCommands
             }
         }
 
-        /// <summary>
-        /// Получить Solid из элемента
-        /// </summary>
-        private void GetSolidsFromGeomElem(GeometryElement geometryElement, Transform transformation, IList<Solid> solids)
+        private void SetParamData(Document doc)
         {
-            foreach (GeometryObject geomObject in geometryElement)
+            foreach (KeyValuePair<ElementId, List<Element>> kvp in _hostForRebarsDict)
             {
-                switch (geomObject)
-                {
-                    case Solid solid:
-                        solids.Add(solid);
-                        break;
-
-                    case GeometryInstance geomInstance:
-                        GetSolidsFromGeomElem(geomInstance.GetInstanceGeometry(), geomInstance.Transform.Multiply(transformation), solids);
-                        break;
-
-                    case GeometryElement geomElem:
-                        GetSolidsFromGeomElem(geomElem, transformation, solids);
-                        break;
-                }
-            }
-        }
-
-        private List<Solid> GetElementSolids(Element element)
-        {
-            List<Solid> solidColl = new List<Solid>();
-
-            Options opt = new Options() { DetailLevel = ViewDetailLevel.Fine };
-            opt.ComputeReferences = true;
-            GeometryElement geomElem = element.get_Geometry(opt);
-            if (geomElem != null)
-            {
-                GetSolidsFromGeomElem(geomElem, Transform.Identity, solidColl);
-            }
-
-            return solidColl.Where(s => s.Volume > 0).ToList();
-        }
-
-        private void SetParamData()
-        {
-            foreach (KeyValuePair<Element, List<Element>> kvp in _hostForRebarsDict)
-            {
-                Element hostElement = kvp.Key;
+                Element hostElement = doc.GetElement(kvp.Key);
                 foreach (Element rebarElement in kvp.Value)
                 {
-                    rebarElement.LookupParameter("СоставнаяМарка").Set(_constrsDB.Where(c => c.Key.EndsWith(hostElement.Id.ToString())).FirstOrDefault().Key);
+                    KGConstruction kgConstr = _kgConstrColl.Where(constr => constr.ConstrCode.EndsWith(hostElement.Id.ToString())).FirstOrDefault();
+                    if (kgConstr == null)
+                        continue;
 
-                    string rebarVolumeParamName = "Арм.ОбъемБетона";
-                    double volumeCubicM = _constrsDB.Where(c => c.Key.EndsWith(hostElement.Id.ToString())).FirstOrDefault().Value;
+                    rebarElement.LookupParameter("СоставнаяМарка").Set(kgConstr.ConstrCode);
+
+                    double volumeCubicM = kgConstr.ConstrVolume;
 #if Debug2020 || Revit2020
                     double volume = Math.Round(UnitUtils.ConvertToInternalUnits(volumeCubicM, DisplayUnitType.DUT_CUBIC_METERS), 3);
 #else
@@ -466,6 +549,7 @@ namespace KPLN_Tools.ExternalCommands
                         new ForgeTypeId("autodesk.revit.parameter:hostVolumeComputed-1.0.0")), 3);
 #endif
 
+                    string rebarVolumeParamName = "Арм.ОбъемБетона";
                     Parameter volumeParam = rebarElement.LookupParameter(rebarVolumeParamName);
                     if (volumeParam == null)
                     {
