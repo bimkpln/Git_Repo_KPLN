0242464b2d5c247987063baf3271f0193b6971fe
Tuleninov Roman <tuleninov.rv@gmail.com>
2025-12-02 09:32:09 +0300

    Плагин для сопоставления размеров семейств элементов узлов

---
+++ KPLN_Tools/KPLN_Tools/Forms/CabinetMismatchWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using KPLN_Tools.ExternalCommands;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KPLN_Tools.Forms
+{
+    public partial class CabinetMismatchWindow : Window
+    {
+        private readonly UIApplication _uiapp;
+
+        public CabinetMismatchWindow(
+            UIApplication uiapp,
+            IList<CabinetMismatchItem> cabinetItems,
+            IList<NodeMismatchItem> nodeItems)
+        {
+            InitializeComponent();
+
+            _uiapp = uiapp;
+            ListBoxCabinets.ItemsSource = cabinetItems;
+            ListBoxNodes.ItemsSource = nodeItems;
+
+            var helper = new System.Windows.Interop.WindowInteropHelper(this)
+            {
+                Owner = System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle
+            };
+        }
+
+        private void GoTo_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn == null)
+                return;
+
+            ElementId id = null;
+
+            CabinetMismatchItem cab = btn.DataContext as CabinetMismatchItem;
+            if (cab != null)
+            {
+                id = cab.ElementId;
+            }
+            else
+            {
+                NodeMismatchItem node = btn.DataContext as NodeMismatchItem;
+                if (node != null)
+                {
+                    id = node.ElementId;
+                }
+            }
+
+            if (id == null)
+                return;
+
+            UIDocument uidoc = _uiapp.ActiveUIDocument;
+            if (uidoc == null)
+                return;
+
+            var ids = new List<ElementId> { id };
+            uidoc.Selection.SetElementIds(ids);
+            uidoc.ShowElements(id);
+        }
+    }
+}
--- KPLN_Tools/KPLN_Tools/Module.cs
+++ KPLN_Tools/KPLN_Tools/Module.cs
@@ -507,8 +507,25 @@ namespace KPLN_Tools
                     "KPLN_Tools.Imagens.FillInParamSmall.png",
                     "http://moodle/mod/book/view.php?id=502&chapterid=1319");
 
+                PushButtonData ssCheckingDimension = CreateBtnData(
+                    "Проверка габаритов ЭУ",
+                    "Проверка габаритов ЭУ",
+                    "Проверка габаритов ЭУ",
+                    string.Format("Задача 1. Проверка параметра ``КП_О_Группирование`` у семейств Элементов узлов и сравнение его с параметром семейств шкафа ``Имя панели``.\n" +
+                    "Задача 2. Проверка габаритов у семейств элементов узлов и семейств шкафа, в случае если параметр КП_О_Группирование = Имя панели\n" +
+                        "Дата сборки: {0}\nНомер сборки: {1}\nИмя модуля: {2}",
+                        ModuleData.Date,
+                        ModuleData.Version,
+                        ModuleData.ModuleName
+                    ),
+                    typeof(Command_CheckingDimensionSS).FullName,
+                    "KPLN_Tools.Imagens.FillInParamSmall.png",
+                    "KPLN_Tools.Imagens.FillInParamSmall.png",
+                    "http://moodle/");
+
                 ssToolsPullDownBtn.AddPushButton(ssSystems);
                 ssToolsPullDownBtn.AddPushButton(ssFillInParameters);
+                ssToolsPullDownBtn.AddPushButton(ssCheckingDimension);
             }
             #endregion
 
