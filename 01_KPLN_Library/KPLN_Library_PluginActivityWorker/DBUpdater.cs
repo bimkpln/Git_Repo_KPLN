3ff28084c48e345c6b90f6f20f7602f6a540aa88
BIM_KPLN <bim@kpln.ru>
2026-02-03 17:46:11 +0300

    1.1.1. Добавлена адаптация имени плагина; перевод на KPLN_Library_SQLiteWorker

--- 01_KPLN_Library/KPLN_Library_PluginActivityWorker/DBUpdater.cs
+++ 01_KPLN_Library/KPLN_Library_PluginActivityWorker/DBUpdater.cs
@@ -1,6 +1,6 @@
 ﻿using KPLN_Library_Forms.UI.HtmlWindow;
+using KPLN_Library_SQLiteWorker;
 using KPLN_Library_SQLiteWorker.Core.SQLiteData;
-using KPLN_Library_SQLiteWorker.FactoryParts;
 using System;
 using System.Threading.Tasks;
 
@@ -8,12 +8,6 @@ namespace KPLN_Library_PluginActivityWorker
 {
     public static class DBUpdater
     {
-        private static readonly UserDbService _userDbService = (UserDbService)new CreatorUserDbService().CreateService();
-        private static readonly ModuleDbService _moduleDbService = (ModuleDbService)new CreatorModuleDbService().CreateService();
-        private static readonly PluginActivityDbService _pluginActivityDbService = (PluginActivityDbService)new CreatorPluginActivityDbService().CreateService();
-
-        private static DBUser _dBUser;
-
         public static async Task UpdatePluginActivityAsync_ByPluginNameAndModuleName(string pluginName, string moduleName)
         {
             try
@@ -33,38 +27,39 @@ namespace KPLN_Library_PluginActivityWorker
 
         private static void UpdatePluginActivity_ByPluginNameAndDirName(string pluginName, string moduleName)
         {
-            // Активирую данные по пользователю (позже переведи на KPLN_Loader, пока рано)
-            if (_dBUser == null)
-                _dBUser = _userDbService.GetCurrentDBUser();
+            // Чистка имени плагина от переноса строк (в имени на вкладке это нужно, в БД - выглядит не очень)
+            string clearPluginName = pluginName.Replace("\n", " ");
+
 
             // Для режима дебага и пользователей из вне - игнор
-            if (_dBUser.IsDebugMode || _dBUser.IsExtraNet) return;
+            if (DBMainService.CurrentDBUser.IsDebugMode || DBMainService.CurrentDBUser.IsExtraNet) return;
 
-            string currentDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm");
+
             // Обработка плагина по отделу и имени
-            DBPluginActivity currentPluginActivity = _pluginActivityDbService.GetDBPluginActivity_ByModuleNameAndSubDep(pluginName, _dBUser.SubDepartmentId);
+            string currentDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm");
+            DBPluginActivity currentPluginActivity = DBMainService.PluginActivityDbService.GetDBPluginActivity_ByModuleNameAndSubDep(clearPluginName, DBMainService.CurrentDBUser.SubDepartmentId);
             if (currentPluginActivity == null)
             {
-                DBModule currentModule = _moduleDbService.GetDBModule_ByFiDirName(moduleName)
+                DBModule currentModule = DBMainService.ModuleDbService.GetDBModule_ByFiDirName(moduleName)
                     ?? throw new Exception($"Не удалось найти модуль по имени '{moduleName}'. Проверь БД, или ошибка при создании плагина.");
 
                 currentPluginActivity = new DBPluginActivity()
                 {
                     ModuleId = currentModule.Id,
-                    PluginName = pluginName,
-                    SubDepartmentId = _dBUser.SubDepartmentId,
+                    PluginName = clearPluginName,
+                    SubDepartmentId = DBMainService.CurrentDBUser.SubDepartmentId,
                     UsageCount = 1,
                     LastActivityDate = currentDate,
                 };
 
-                _pluginActivityDbService.CreateDBPluginActivity(currentPluginActivity);
+                DBMainService.PluginActivityDbService.CreateDBPluginActivity(currentPluginActivity);
                 return;
             }
 
             currentPluginActivity.UsageCount++;
             currentPluginActivity.LastActivityDate = currentDate;
 
-            _pluginActivityDbService.UpdatePluginActivity_ByPluginActivityAndSubDep(currentPluginActivity, _dBUser.SubDepartmentId);
+            DBMainService.PluginActivityDbService.UpdatePluginActivity_ByPluginActivityAndSubDep(currentPluginActivity, DBMainService.CurrentDBUser.SubDepartmentId);
         }
     }
 }
