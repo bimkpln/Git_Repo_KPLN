dc66d9b53ed381f7457421fe55135378daa7e425
Tuleninov Roman <tuleninov.rv@gmail.com>
2026-02-25 10:46:26 +0300

    Плагин для автомоделирования путей эвакуации. Начальный макет

---
+++ KPLN_Tools/KPLN_Tools/ExternalCommands/Command_AR_EvacuationRoutes.cs
@@ -0,0 +1,48 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using View = Autodesk.Revit.DB.View;
+
+
+namespace KPLN_Tools.ExternalCommands
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    internal class Command_AR_EvacuationRoutes : IExternalCommand
+    {
+        private static UIApplication uiapp;
+        UIDocument uidoc;
+        Document doc;
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            uiapp = commandData.Application;
+            uidoc = uiapp.ActiveUIDocument;
+            doc = uidoc.Document;
+
+            try
+            {
+                createEvacuationRoutes();
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                TaskDialog.Show("Ошибка", ex.Message);
+                return Result.Failed;
+            }
+            
+        }
+
+
+
+        public void createEvacuationRoutes()
+        {
+
+        }
+    }
+}
