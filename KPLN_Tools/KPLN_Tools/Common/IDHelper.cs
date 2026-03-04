a8c50e09b73453b4149514cc88f2a4af755ff789
BIM_KPLN <bim@kpln.ru>
2026-03-04 12:16:25 +0300

    1.14.1. Релиз нового плагина - автомоделирование путей эвакуации

---
+++ KPLN_Tools/KPLN_Tools/Common/IDHelper.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPLN_Tools.Common
+{
+    internal static class IDHelper
+    {
+        internal static long EidValue(ElementId id)
+        {
+#if Revit2024 || Debug2024
+            return id.Value;
+#else
+    return id.IntegerValue;
+#endif
+        }
+
+        internal static int EidInt(ElementId id)
+        {
+#if Revit2024 || Debug2024
+            return (int)id.Value;
+#else
+            return id.IntegerValue;
+#endif
+        }
+
+    }
+}
