3cdd8330a9a62140c727ed2d4098578b88c46ed1
Tuleninov Roman <tuleninov.rv@gmail.com>
2026-06-22 11:11:29 +0300

    Штурвал команд. Инициализация

---
+++ KPLN_CommandsWheel/ExternalCommands/WheelSettings.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace KPLN_CommandsWheel.ExternalCommands
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class WheelSettings : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Получение объектов приложения и документа
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            //Тест
+            TaskDialog.Show("TEST", "Настройка", TaskDialogCommonButtons.Ok);
+            return Result.Succeeded;
+        }
+    }
+}
