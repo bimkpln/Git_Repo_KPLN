6b059e1f99d57b818cc9e485743766e8ab685775
Tuleninov Roman <tuleninov.rv@gmail.com>
2026-07-08 07:43:04 +0300

    Сбор информации по параметрам семейства. Инициализация

---
+++ KPLN_BIMTools_Ribbon/KPLN_BIMTools_Ribbon/ExternalCommands/CommandCollectionFamilyParameters.cs
@@ -0,0 +1,34 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using KPLN_BIMTools_Ribbon.Forms;
+using System;
+using System.Windows.Interop;
+
+namespace KPLN_BIMTools_Ribbon.ExternalCommands
+{
+    [Transaction(TransactionMode.Manual)]
+    internal class CommandCollectionFamilyParameters : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                TaskDialog.Show("Тест", "ТЕСТ");
+                return Result.Succeeded;
+
+                CollectionParametersWindow window = new CollectionParametersWindow(commandData);
+                WindowInteropHelper helper = new WindowInteropHelper(window);
+                helper.Owner = commandData.Application.MainWindowHandle;
+                window.Show();
+
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+        }
+    }
+}
